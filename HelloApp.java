836800bc252988348d41616a5a875e4f81399a81
MohanaReddipalli-Dev <mohanareddipalli@gmail.com>
2026-03-17 09:38:08 +0530

    UC4 printing HelloApp using MultipleCommands

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
-
-        
-        if (args.length > 0) {
-            
-            name = args[0];
+        // Check if command-line arguments are provided
+        if (args.length == 0) {
+            // No arguments, display default greeting
+            System.out.println("Hello, World!");
         } else {
-            
-            name = "World";
+            // Arguments exist, join all names with commas and spaces
+            String names = String.join(", ", args);
+            // Display personalized greeting with all names
+            System.out.println("Hello, " + names + "!");
         }
-
-        
-        System.out.println("Hello, " + name + "!");
     }
 }
