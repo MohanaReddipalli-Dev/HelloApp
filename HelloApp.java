50f7944d1c66e839a253085a4d6b9b657a212c46
MohanaReddipalli-Dev <mohanareddipalli@gmail.com>
2026-03-25 08:58:27 +0530

    UC5 printing HelloApp using loop

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length == 0) {
-            
+           
             System.out.println("Hello, World!");
         } else {
             
